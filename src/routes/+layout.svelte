4717a6d87bae9f37d7100e2a4f5f8354e9a77955
Brandon <brandon.worthington@proton.me>
2024-12-05 15:22:39 -0500

    vercel speed attempt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,11 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	inject({ mode: dev ? 'development' : 'production' });
+
+	injectSpeedInsights();
 
 	let { data, children } = $props();
 </script>
