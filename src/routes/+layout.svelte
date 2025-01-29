8e17fe3d06ab5da7ff8585077764a32fa3fc10d3
Brandon <brandon.worthington@proton.me>
2025-01-29 13:23:16 -0500

    trying to fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.postcss';
 
 	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { page } from '$app/state';
+	import { page } from '$app/stores';
 	import { repositoryName } from '$lib/prismicio';
 	import Header from '$lib/components/Header.svelte';
 
@@ -15,15 +15,15 @@
 </script>
 
 <svelte:head>
-	<title>{page.data.title}</title>
-	{#if page.data.meta_description}
-		<meta name="description" content={page.data.meta_description} />
+	<title>{$page.data.title}</title>
+	{#if $page.data.meta_description}
+		<meta name="description" content={$page.data.meta_description} />
 	{/if}
-	{#if page.data.meta_title}
-		<meta name="og:title" content={page.data.meta_title} />
+	{#if $page.data.meta_title}
+		<meta name="og:title" content={$page.data.meta_title} />
 	{/if}
-	{#if page.data.meta_image}
-		<meta name="og:image" content={page.data.meta_image.url} />
+	{#if $page.data.meta_image}
+		<meta name="og:image" content={$page.data.meta_image.url} />
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 </svelte:head>
