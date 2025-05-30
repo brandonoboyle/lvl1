b317e72c22b245f1329434c998383a7f1dd148a2
Brandon <brandon.worthington@proton.me>
2025-05-30 15:29:13 -0400

    bg colour fix

--- src/lib/slices/BgPageSplitCards/index.svelte
+++ src/lib/slices/BgPageSplitCards/index.svelte
@@ -9,17 +9,17 @@
 	let { slice }: Props = $props();
 </script>
 
-<div class="pb-12">
+<div class="pb-12 px-10">
 	<div class="relative w-full px-12 text-3xl md:text-lg">
 		<div class="">
 			{#if isFilled.image(slice.primary.background)}
 				<PrismicImage
 					field={slice.primary.background}
 					alt=""
-					class="absolute -z-10 inset-0 left-1/2 h-full w-full -translate-x-1/2 select-none rounded-xl object-cover opacity-40 px-8"
+					class="absolute -z-10 inset-0 left-1/2 h-full w-full -translate-x-1/2 select-none rounded-xl object-cover opacity-40"
 				/>
 				<div
-					class="absolute inset-0 left-1/2 h-full w-11/12 -translate-x-1/2 bg-gradient-to-br from-tertiary-400/30 to-secondary-400/30 via-surface-400/30 mix-blend-overlay"
+					class="absolute inset-0 left-1/2 h-full w-full -translate-x-1/2 bg-gradient-to-br from-tertiary-400/30 to-secondary-400/30 via-surface-400/30 mix-blend-overlay rounded-xl"
 				></div>
 			{/if}
 		</div>
