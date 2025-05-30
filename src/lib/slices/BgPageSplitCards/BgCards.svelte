6582035cd2f861064b24df238b2c3c6f774b446d
Brandon <brandon.worthington@proton.me>
2025-05-30 15:25:18 -0400

    spacing fix

--- src/lib/slices/BgPageSplitCards/BgCards.svelte
+++ src/lib/slices/BgPageSplitCards/BgCards.svelte
@@ -16,7 +16,7 @@
 	<PrismicLink field={card.card_link}>
 		<!--				Inner text box-->
 
-		<div class="flex flex-col items-center justify-center rounded-xl bg-surface-800 min-h-64 min-w-[280px] max-w-[400px] px-8 py-8 text-xl md:px-12 md:py-12 transition-shadow duration-300 ease-in-out [@media(hover:hover)]:hover:shadow-lg [@media(hover:hover)]:hover:shadow-tertiary-600">
+		<div class="flex flex-col items-center justify-center rounded-xl bg-surface-800 min-h-64 px-8 py-8 text-xl md:px-12 md:py-12 transition-shadow duration-300 ease-in-out [@media(hover:hover)]:hover:shadow-lg [@media(hover:hover)]:hover:shadow-tertiary-600">
 			<span class="text-center bg-gradient-to-br from-primary-500 to-tertiary-400 box-decoration-clone bg-clip-text text-transparent"><PrismicRichText field={card.card_title} /></span>
 			<span class="text-center"><PrismicRichText field={card.card_content} /></span>
 		</div>
