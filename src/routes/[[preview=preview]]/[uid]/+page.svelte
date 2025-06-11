754ad717a206ca456737cfdf66c84cd299fe0f54
Brandon <brandon.worthington@proton.me>
2025-06-11 11:44:40 -0400

    preview metadata added

--- src/routes/[[preview=preview]]/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/[uid]/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import { SliceZone } from '@prismicio/svelte';
+	import { page } from '$app/stores';
 
 	import { components } from '$lib/slices';
 
 	let { data } = $props();
 </script>
+
+<svelte:head>
+	<title>{$page.data.title}</title>
+	{#if $page.data.meta_description}
+		<meta name="description" content={$page.data.meta_description} />
+	{/if}
+	{#if $page.data.meta_title}
+		<meta name="og:title" content={$page.data.meta_title} />
+	{/if}
+	{#if $page.data.meta_image}
+		<meta name="og:image" content={$page.data.meta_image.url} />
+		<meta name="twitter:card" content="summary_large_image" />
+	{/if}
+</svelte:head>
 
 <SliceZone slices={data.page.data.slices} {components} />
